33a67ca7fc7aa1ffc61665927440643272feaff2
Adrien Philippe <adrien.philippe@ubisoft.com>
2019-07-22 14:13:47 +0200

    update build scripts : copy index.html and create package.json in bin folder at start and build script

--- build_scripts/nw.js
+++ build_scripts/nw.js
@@ -1,18 +1,27 @@
+const fs = require('fs');
+
 if (process.argv.length === 2) {
     console.error('Expected at least one argument!');
     process.exit(1);
 }
 
-var NwBuilder = require('nw-builder');
-var nw = new NwBuilder({
-    files: './bin/**', // use the glob format
-    platforms: ['win64'],
-    version: '0.38.4'
-});
+var func = {
+    "-r" : run,
+    "-b" : build
+}[process.argv[2]];
 
-nw.on('log', console.log);
+if (func == null)
+{
+    console.log("error use -r or -b arguments");
+}
 
-if (process.argv[2] === '-r') {
+createPackageFile();
+copyHtmlFiles();
+
+var builder = createBuilder();
+func(builder);
+
+function run(nw) {
     nw.run().then(function () {
         console.log('all done!');
     }).catch(function (error) {
@@ -20,11 +29,40 @@ if (process.argv[2] === '-r') {
     });
 }
 
-if (process.argv[2] === '-b') {
+function build(nw) {
     nw.options.flavor = "normal";
+
     nw.build().then(function () {
         console.log('all done!');
     }).catch(function (error) {
         console.error(error);
     });
+}
+
+function createBuilder() {
+    var NwBuilder = require('nw-builder');
+    var nw = new NwBuilder({
+        files: './bin/**', // use the glob format
+        platforms: ['win64'],
+        version: '0.38.4'
+    });
+
+    nw.on('log', console.log);
+
+    return nw;
+}
+
+function createPackageFile () {
+    var package = {
+        "name" : process.env.npm_package_name,
+        "version" : process.env.npm_package_version,
+        "main": "index.html"
+    }
+
+    fs.writeFileSync("./bin/package.json", JSON.stringify(package), 'utf-8');
+}
+
+function copyHtmlFiles()
+{
+    fs.copyFileSync("./html/index.html", "./bin/index.html");
 }
